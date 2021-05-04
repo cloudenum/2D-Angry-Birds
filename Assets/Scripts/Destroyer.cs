2962ce62fdc742fe9074e6ccc82d46b4a0536bb7
cloudenum <23111954+cloudenum@users.noreply.github.com>
2021-05-04 20:50:36 +0700

    initial commit

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.gameObject.tag;
+        
+        if (tag == "Obstacle" || tag == "Bird")
+        {
+            Destroy(col.gameObject);
+        }
+        else if (tag == "Enemy")
+        {
+            col.GetComponent<Enemy>().outOfBounds();
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health = 50f;
+
+    public UnityAction<GameObject> OnEnemyDestroyed = delegate { };
+
+    private bool _isHit = false;
+
+    void OnDestroy()
+    {
+        if (_isHit)
+        {
+            OnEnemyDestroyed(gameObject);
+        }
+    }
+
+    public void outOfBounds()
+    {
+        _isHit = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        if (col.gameObject.CompareTag("Bird"))
+        {
+            _isHit = true;
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.CompareTag("Obstacle"))
+        {
+            float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+        else if (col.gameObject.CompareTag("Enemy"))
+        {
+            float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 5;
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
